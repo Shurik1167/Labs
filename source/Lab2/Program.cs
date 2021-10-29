78e1c23452c6fd7b7c5739fd53d8e128cd266847
Aleksandr Kovalev <alexkovalev6711@gmail.com>
2021-10-29 23:52:55 +0300

    Refactoring

--- source/Lab2/Program.cs
+++ source/Lab2/Program.cs
@@ -4,12 +4,12 @@ namespace Lab2
 {
     class Program
     {
-        static void WR(int[] outputData)
+        static void OutputData(int[] data)
         {
-            Console.WriteLine(string.Join(',', outputData));
+            Console.WriteLine(string.Join(',', data));
         }
 
-        static void Squ(int[] inputData)
+        static void ToSquares(int[] inputData)
         {
             for (int k = 0; k < inputData.Length; k++)
             {
@@ -43,15 +43,20 @@ namespace Lab2
             return;
         }
 
+        static void ToSquaresAndQuickSort(int[] data, int first, int last)
+        {
+            ToSquares(data);
+            QuickSort(data, first, last);
+        }
+
         static void Main()
         {
             int[] inputData = new int[] {-2, 45, 5, -12, 10};
+            OutputData(inputData);
 
-            WR(inputData);
-            Squ(inputData);
-            WR(inputData);
-            QuickSort(inputData, 0, inputData.Length - 1);
-            WR(inputData);
+            ToSquaresAndQuickSort(inputData, 0, inputData.Length - 1);
+
+            OutputData(inputData);
         }
     }
 }
